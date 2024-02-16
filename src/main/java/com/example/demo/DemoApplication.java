478128afdb692d0cdb905abea90f6d9b0de8cd0e
Marcelo Romulo Fernandes <persapiens@gmail.com>
2024-02-15 21:15:36 -0300

    comment with wrong characters after save inside visual code with auto format on save.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot is fantastic. (pt-br) Spring Boot ? fant?stico.
+ */
 @SpringBootApplication
 public class DemoApplication {
 
